432f53f424a3951806f597fb12a51c9f2c188fb4
Cooper Labinger <clabinger@gmail.com>
2020-10-23 10:13:09 +0000

    Lint: update spacing

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ const firestore = admin.firestore();
 
 const time_threshold = 1; // Do not reply more than once within 5 minutes
 
-const formatPhone = function(rawPhoneNumber, formatCode) {
+const formatPhone = function (rawPhoneNumber, formatCode) {
 	// formatCode: 1 = numbers only, 2 = Twilio, default = human
 
 	let phoneNumber = rawPhoneNumber.replace(/\D+/g, '');
@@ -31,9 +31,9 @@ const formatPhone = function(rawPhoneNumber, formatCode) {
 	}	
 }
 
-const sendMessage = function(from, to, body, mediaUrls, callback){
+const sendMessage = function (from, to, body, mediaUrls, callback) {
 
-	console.log('Sending message to '+to+': "'+body+'". Sending...');
+	console.log('Sending message to ' + to + ': "' + body + '". Sending...');
 
 	const client = require('twilio')(functions.config().twilio.account_sid, functions.config().twilio.auth_token); 
 
@@ -43,45 +43,45 @@ const sendMessage = function(from, to, body, mediaUrls, callback){
 	    body: body
 	}
 
-	if(mediaUrls){
+	if (mediaUrls) {
 		parameters.mediaUrl = mediaUrls;
 	}
 
-	client.messages.create(parameters, function(err, message){
+	client.messages.create(parameters, function (err, message) {
 	    
-	    if(err!==null){
-	    	console.error('Message to '+to+' not sent successfully: '+err);
+	    if (err !== null) {
+	    	console.error('Message to ' + to + ' not sent successfully: ' + err);
 	    	console.info(message.sid);
-	    }else{
-	    	console.log('Message to '+to+' sent successfully.');
+	    } else {
+	    	console.log('Message to ' + to + ' sent successfully.');
 	    }
 
-	    if(typeof callback === 'function'){
-	    	callback((err===null));
+	    if (typeof callback === 'function') {
+	    	callback((err === null));
 	    }
 	});
 
 }
 
-const receiveMessage = function(options){
+const receiveMessage = function (options) {
 
 	return functions.https.onRequest((req, res) => {
 
 		const third_party_number = req.body['From'];
 		const third_party_message = req.body['Body'];
 
-		console.log('Receiving message from '+third_party_number+': "'+third_party_message+'".');
+		console.log('Receiving message from ' + third_party_number + ': "' + third_party_message + '".');
 
-	    const forward_message = 'Verizon Fwd from '+formatPhone(third_party_number)+': '+third_party_message;
+	    const forward_message = 'Verizon Fwd from ' + formatPhone(third_party_number) + ': ' + third_party_message;
 
 	    // Forward message to new number
 
 	    let mediaUrls = null;
 
-		if(req.body['NumMedia']>0){
+		if (req.body['NumMedia'] > 0) {
 			mediaUrls = [];
-			for(let i=0; i<req.body['NumMedia']; i++){
-				mediaUrls.push(req.body['MediaUrl'+i]);
+			for (let i = 0; i < req.body['NumMedia']; i++) {
+				mediaUrls.push(req.body['MediaUrl' + i]);
 			}
 		}
 
@@ -90,7 +90,7 @@ const receiveMessage = function(options){
 			options.new_number, 
 			forward_message, 
 			mediaUrls,
-			function(successful){ // Reply to third party
+			function (successful) { // Reply to third party
 
 				console.log('Replying to 3rd party...');
 
@@ -98,28 +98,28 @@ const receiveMessage = function(options){
 
 				// Only reply if they have not gotten a reply in the last x minutes
 
-				var numberRef = admin.database().ref('/replies/'+formatPhone(third_party_number, 1)+'/time');
-				var messageRef = admin.database().ref('/incoming/'+formatPhone(third_party_number, 1));
+				var numberRef = admin.database().ref('/replies/' + formatPhone(third_party_number, 1) + '/time');
+				var messageRef = admin.database().ref('/incoming/' + formatPhone(third_party_number, 1));
 
-				numberRef.once('value').then(function(snapshot){
+				numberRef.once('value').then(function (snapshot) {
 					const lastTime = snapshot.val();
 
 			        const incoming_message_test = third_party_message.trim().toLowerCase();
 
-			        const requested_number = (incoming_message_test==='number');
+			        const requested_number = (incoming_message_test === 'number');
 
 			        const time_since = thisTime - lastTime;
 
-				    messageRef.push({message: third_party_message});
+				    messageRef.push({ message: third_party_message });
 
-					if(requested_number || !lastTime || time_since > (time_threshold * 60 * 1000)){ // x minutes, 60 seconds per minute, 1000 millseconds per second
+					if (requested_number || !lastTime || time_since > (time_threshold * 60 * 1000)) { // x minutes, 60 seconds per minute, 1000 millseconds per second
 				        
 				        let reply_message = '';
 
-				        if(requested_number){
+				        if (requested_number) {
 				            reply_message = formatPhone(options.new_number);
-				        }else{
-				        	reply_message = 'I have a new mobile phone number. Please reply NUMBER to get the new number and update your address book. Your original message has'+(successful ? '':' NOT')+' been forwarded. Thank you. --'+options.name;
+				        } else {
+				        	reply_message = 'I have a new mobile phone number. Please reply NUMBER to get the new number and update your address book. Your original message has' + (successful ? '':' NOT') + ' been forwarded. Thank you. --' + options.name;
 				        }
 
 				        sendMessage(
@@ -129,15 +129,15 @@ const receiveMessage = function(options){
 					    );
 
 					    // Save in db that we sent message to this person
-					    numberRef.set(thisTime, function(error){
-					    	if(error){
-					    		console.error('Sent message was NOT saved to the database: '+error);
-					    	}else{
+					    numberRef.set(thisTime, function (error) {
+					    	if (error) {
+					    		console.error('Sent message was NOT saved to the database: ' + error);
+					    	} else {
 					    		console.log('Sent message saved to database.');
 					    	}
 					    });
-					}else{
-						console.log('Already replied to '+third_party_number+' within '+time_threshold+' minutes, not replying.');
+					} else {
+						console.log('Already replied to ' + third_party_number + ' within ' + time_threshold + ' minutes, not replying.');
 					}
 
 				});
@@ -150,9 +150,9 @@ const receiveMessage = function(options){
 };
 
 // Build out exported functions for each instance
-for(let i in functions.config().instances){
+for (let i in functions.config().instances) {
 	let options = functions.config().instances[i];
-	exports['receiveMessage_'+i] = receiveMessage(options);
+	exports['receiveMessage_' + i] = receiveMessage(options);
 }
 
 
@@ -160,7 +160,7 @@ exports.addMessage = functions.https.onRequest((req, res) => {
 
   const message = req.query['Body'];
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  admin.database().ref('/incoming').push({message: message}).then(snapshot => {
+  admin.database().ref('/incoming').push({ message: message }).then(snapshot => {
     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
     res.redirect(303, snapshot.ref);
   });
